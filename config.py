4018ca0d505f4bc04f66ca617ffac6d7ebf6dab7
Mr Broken <74880754+asad0111@users.noreply.github.com>
2024-05-26 05:47:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,19 +4,19 @@
 import os
 from os import getenv
 # ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID", "16874790"))
+API_ID = int(os.environ.get("API_ID", "20104370"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH", "46aa49adca0f1d184eb2a2f4a48a1df9")
+API_HASH = os.environ.get("API_HASH", "1ee68fa02aa69ec187bb2854d19bca9c")
 # ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6770739862:AAEJnHU9R-B9vB71xWoh6VfvqMFAzwJc-Lw")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7120981362:AAGlWbWVdcDkrEVhqkbvMhjayRrk9tGUPI0")
 # -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "EQUROBOT")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "EQURO_BOT")
 # ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "6664582540"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "1841914911"))
 # ------------------X------------------------------
 DEEP_API = os.environ.get("DEEP_API", "5163c49d-b696-47f1-8cf9-4801738436dd")
 # ------------------------------------------------
-LOGGER_ID = int(os.environ.get("LOGGER_ID", "-1001919135283"))
+LOGGER_ID = int(os.environ.get("LOGGER_ID", "-1002094142057"))
 # ------------------------------------------------
 GPT_API = os.environ.get("GPT_API", "696-47f1-8cf9-4801738436dd")
 # ------------------------------------------------
