d3b696168041e7ea636c57ab01d9df6e0244349c
Mr Broken <74880754+asad0111@users.noreply.github.com>
2024-05-26 10:48:16 +0530

    Update alive.py

--- EQUROBOT/modules/alive.py
+++ EQUROBOT/modules/alive.py
@@ -2,6 +2,6 @@ from pyrogram import Client, filters
 from EQUROBOT import app
 
 # ---------------------------
-@app.on_message(filters.command("alive", prefixes="."))
+@app.on_message(filters.command("alive", prefixes="","."))
 def alive(_, message):
-    message.reply_text("**🥀 I Aᴍ Aʟɪᴠᴇ Mʏ Dᴇᴀʀ Gᴇɴɪᴜs Mᴀsᴛᴇʀ ✨ ...**")
+    message.reply_text("**🥀 I Aᴍ Aʟɪᴠᴇ Mʏ Dᴇᴀʀ Gᴇɴɪᴜs Mᴀsᴛᴇʀ 😈 ...**")
