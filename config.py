bcc712de4325d7b4f10b1b6bbff661e5d35dfd4c
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-06-13 20:06:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,24 @@
 
-# --------------M----------------------------------
+# ----------------M---------R-----------------------
 
 import os
 from os import getenv
-# ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID", "20104370"))
+# -------------------B-----------------------------
+API_ID = int(os.environ.get("API_ID", "")) #⚠ FILL
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH", "1ee68fa02aa69ec187bb2854d19bca9c")
-# ----------------D--------------------------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7258769781:AAFIGqv9FFRVjlZqxam-dqSbdfoWaFtsgoo")
-# -----------------A-------------------------------
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "EQURO_BOT")
-# ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", "1841914911"))
-# ------------------X------------------------------
-DEEP_API = os.environ.get("DEEP_API", "5163c49d-b696-47f1-8cf9-4801738436dd")
+API_HASH = os.environ.get("API_HASH", "")         #FILL
+# ----------------------R--------------------------
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7258769781:dqSbdfoWaFtsgoo") #CHANGE
 # ------------------------------------------------
-LOGGER_ID = int(os.environ.get("LOGGER_ID", "-1002094142057"))
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "") #FILL
+# -----------------------O-------------------------
+OWNER_ID = int(os.environ.get("OWNER_ID", "1841914911"))   #OWNER OR SUDO
 # ------------------------------------------------
-GPT_API = os.environ.get("GPT_API", "696-47f1-8cf9-4801738436dd")
+DEEP_API = os.environ.get("DEEP_API", "5163c49d-b696-47f1-8cf9-4801738436dd") #GET YOUR @DEEPAI.ORG
+# -------------------------K-----------------------
+LOGGER_ID = int(os.environ.get("LOGGER_ID", "-1002094142057")) #REPLACE WITH YOUR LOGGER
+# ----------------------------E--------------------
+GPT_API = os.environ.get("GPT_API", "696-47f1-8cf9-4801738436dd") #REPLACE WITH YOUR GPT_API
 # ------------------------------------------------
-DAXX_API = os.environ.get("DAXX_API", "5163c49d-b696-47f1-8cf9-")
+DAXX_API = os.environ.get("DAXX_API", "5163c49d-b696-47f1-8cf9-")   #NO NEED TO CHANGE
+# --------------------------------N------------------
