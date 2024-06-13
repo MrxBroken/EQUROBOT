7c963ce69ce6494676350e1f29d61da828a4b190
Mr Broken <74880754+mrxbroken011@users.noreply.github.com>
2024-06-13 18:13:02 +0530

    Update __main__.py

--- EQUROBOT/__main__.py
+++ EQUROBOT/__main__.py
@@ -14,9 +14,9 @@ async def daxxpapa_boot():
     for all_module in ALL_MODULES:
         importlib.import_module("EQUROBOT.modules." + all_module)
     print("𝖻𝗈𝗍 𝗌𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝗌𝗍𝖺𝗋𝗍")
-    await app.send_message(LOGGER_ID, "**𝖨 𝖺𝗆 𝖺𝗅𝗂𝗏𝖾 𝖡𝖺𝖻𝗒 𝖸𝗈𝗎𝗋 𝖡𝗈𝗍 𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝖣𝖾𝗉𝗅𝗈𝗒 \n Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ  [➤𝗠𝗥 𝗫 𝗕𝗥𝗢𝗞𝗘𝗡 ](https://t.me/MRBROKN)**")
+    await app.send_message(LOGGER_ID, "**𝖨 𝖺𝗆 𝖺𝗅𝗂𝗏𝖾 𝖡𝖺𝖻𝗒 𝖸𝗈𝗎𝗋 𝖡𝗈𝗍 𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅 𝖣𝖾𝗉𝗅𝗈𝗒 \n Mʏ Dᴇᴠᴇʟᴏᴘᴇʀ  [**ᴍʀ ʙʀᴏᴋᴇɴ**](https://t.me/MRBROKN)**")
     await idle()
-    print("𝗠𝗔𝗗𝗘 𝗕𝗬 ➤𝗠𝗥 𝗫 𝗕𝗥𝗢𝗞𝗘𝗡")
+    print("𝗠𝗔𝗗𝗘 𝗕𝗬 𝗠𝗥 𝗫 𝗕𝗥𝗢𝗞𝗘𝗡")
   
   
 if __name__ == "__main__":
